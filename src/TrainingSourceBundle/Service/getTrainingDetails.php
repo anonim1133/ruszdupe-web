9de84f02581676ec80c4227954bb2e977da00f46
Krzysztof Oleśków <anonim1133@wp.pl>
2015-07-31 12:29:44 +0200

    Unified strava and endomondo results

--- src/TrainingSourceBundle/Service/getTrainingDetails.php
+++ src/TrainingSourceBundle/Service/getTrainingDetails.php
@@ -17,23 +17,26 @@ class getTrainingDetails{
 	$this->_runkeeper = $runkeeper;
     }
     
-    function setUrl($url){
-	$this->_url = $url;
-	
+    private function setUrl($url){
+	$this->_url = trim($url);
+
 	if(preg_match('/endo/', $url)){
 	    $training = $this->_endomondo->get($this->_url);
 	    
 	    $this->_training['training'] = $training;
+	    $this->_training['url'] = $this->_url;
 	    
 	    $this->_training['start_time'] = $training->local_start_time;
-	    $this->_training['duration'] = $training->duration;
+	    $this->_training['duration'] = round($training->duration);
 	    $this->_training['distance'] = $training->distance;
 	    $this->_training['distance_vertical'] = $training->ascent+$training->descent;
-	    $this->_training['calories'] = $training->calories;
+	    $this->_training['calories'] = round($training->calories);
 	    $this->_training['speed_avg'] = $training->speed_avg;
 	    $this->_training['speed_max'] = $training->speed_max;
-	    $this->_training['heart_rate_avg'] = $training->heart_rate_avg;
-	    $this->_training['heart_rate_max'] = $training->heart_rate_max;
+	    if(isset($training->heart_rate_avg))
+		$this->_training['heart_rate_avg'] = $training->heart_rate_avg;
+	    if(isset($training->heart_rate_max))
+		$this->_training['heart_rate_max'] = $training->heart_rate_max;
 
 	    if(isset($training->pictures))
 		$this->_training['pictures'] = $training->pictures;
@@ -42,27 +45,26 @@ class getTrainingDetails{
 	    $training = $this->_strava->get($this->_url);
 	    
 	    $this->_training['training'] = $training;
+	    $this->_training['url'] = $this->_url;
 	    
 	    $this->_training['start_time'] = $training->start_date;
-	    
-	    $date = new \DateTime();
-	    $date->setTimeStamp($training->elapsed_time/10);
-	    $this->_training['duration'] = $date->format('H:i:s');
-	    $this->_training['distance'] = $training->distance;
-	    $this->_training['calories'] = $training->kilojoules*0.2388458966;
+	    $this->_training['duration'] = round($training->elapsed_time);
+	    $this->_training['distance'] = $training->distance/1000;
+	    $this->_training['calories'] = round($training->kilojoules*02.388458966);
 	    $this->_training['speed_avg'] = $training->average_speed;
 	    $this->_training['speed_max'] = $training->max_speed;
-	    $this->_training['heart_rate_avg'] = $training->average_heartrate;
-	    $this->_training['heart_rate_max'] = $training->max_heartrate;
-	    
-	//}elseif(preg_match('/endo/', $url)){
-	//    $this->_training = $this->_runkeeper->get($this->_url);
+	    if(isset($training->average_heartrate))
+		$this->_training['heart_rate_avg'] = $training->average_heartrate;
+	    if(isset($training->max_heartrate))
+		$this->_training['heart_rate_max'] = $training->max_heartrate;
 	}else{
-	    throw new Exception('Unrecognized url');
+	    $this->_training['distance'] = (int)$url;
 	}
     }
     
-    function get(){
+    function get($url){
+	$this->_training = array();
+	$this->setUrl($url);
 	return $this->_training;
     }
 }
